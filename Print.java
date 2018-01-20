cddeddb72b7075d7ad8a33ffb18f80d2d2e4fe42
jfshimul <shimul39ju@gmail.com>
2018-01-21 01:07:39 +0600

    3rd

--- Print.java
+++ Print.java
@@ -4,7 +4,9 @@ public class Print
  public static void main(String args[])
     {
     // System.out.println("Hello\nWorld");
-    System.out.println(3+4+5);
-    System.out.println("3 + 4 + 5");
+    // System.out.println(3+4+5);
+    // System.out.println("3 + 4 + 5");
+    System.out.print(3);
+    System.out.println(4+5);
     }
 }
